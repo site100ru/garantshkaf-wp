a6a2407c78845731b93c8ca2306e292238b2017f
Slava_web_student <84717237+SlavaWebDeveloper@users.noreply.github.com>
2026-02-19 22:27:57 +0700

    19.02 (#47)
    * favicon
    
    * изменил дату акций на следующий год
    
    * замена изображений

--- shkafy-cupe.php
+++ shkafy-cupe.php
@@ -346,7 +346,7 @@ include 'header.php';
 				<div class="col-12 col-md-6 mb-3 mb-md-0 section-image order-1 order-md-0">
 					<div class="single-image-wrapper">
 						<div class="approximation img-wrapper position-relative gallery-2691">
-							<img src="<?php echo get_template_directory_uri(); ?>/img/card/action-card.jpg" class="d-block w-100 single-product-img rounded-large" loading="lazy" alt="Скидка до 25%. Акция до 31 января">
+							<img src="<?php echo get_template_directory_uri(); ?>/img/card/action-card.webp" class="d-block w-100 single-product-img rounded-large" loading="lazy" alt="Скидка до 25%. Акция до 28 февраля">
 						</div>
 					</div>
 				</div>
@@ -354,8 +354,8 @@ include 'header.php';
 				<div class="col-12 col-md-6 mb-0 section-image order-1 order-md-0">
 					<div class="single-image-wrapper">
 						<div class="approximation img-wrapper position-relative gallery-2691">
-							<img src="<?php echo get_template_directory_uri(); ?>/img/card/action-card-2.jpg"
-								class="d-block w-100 single-product-img rounded-large" loading="lazy" alt="Беспроцентная рассрочка на 1 год. Акция до 31 января">
+							<img src="<?php echo get_template_directory_uri(); ?>/img/card/action-card-2.webp"
+								class="d-block w-100 single-product-img rounded-large" loading="lazy" alt="Беспроцентная рассрочка на 1 год. Акция до 28 февраля">
 						</div>
 					</div>
 				</div>
